3c351a7dcb0150610b3d6b2a49888085b107f384
Sunny NG <ngsanluk@gmail.com>
2022-04-18 17:14:08 +0800

    first commit

---
+++ ListDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListDemo.Helpers;
+using Xamarin.Forms;
+
+namespace ListDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        FirebaseHelper fbhelper = new FirebaseHelper();
+        List<Models.Shop> allShops;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            allShops = await fbhelper.GetAllShops();
+            MyListView.ItemsSource = await fbhelper.GetAllShops();
+        }
+    }
+}
---
+++ ListDemo/Models/Shop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace ListDemo.Models
+{
+    public partial class Shop
+    {
+        [JsonProperty("ShopName")]
+        public string ShopName { get; set; }
+
+        [JsonProperty("District")]
+        public string District { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("Latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("Longitude")]
+        public string Longitude { get; set; }
+    }
+
+    public partial class Shop
+    {
+        public static Shop FromJson(string json) => JsonConvert.DeserializeObject<Shop>(json, ListDemo.Models.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Shop self) => JsonConvert.SerializeObject(self, ListDemo.Models.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
